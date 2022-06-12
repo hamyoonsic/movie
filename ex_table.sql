75091ccc6a521ef1d36e1199ba14e1475f2a248e
tlsrl6427 <68215429+tlsrl6427@users.noreply.github.com>
2022-06-12 17:01:56 +0900

    ex_table

---
+++ ex_table.sql
@@ -0,0 +1,36 @@
+/*
+	---------------------------------------------user_info--------
+	
+	insert into user_info values('hong1234', '1234', '홍신기', '900000-1111111', 
+								'010-4930-5764', 'tlsrl6427@naver.com')
+	insert into user_info values('zz', '1111', 'John Doe', '811111-1234567',
+								'010-1234-5678', 'zz@gmail.com')
+	
+	select * from user_info
+	---------------------------------------------ticketing--------
+	
+	
+	
+	---------------------------------------------theater-------
+	
+	insert into theater values(seq_theater_idx.nextVal, '강남', 1, 2, '서초구');
+	insert into theater values(seq_theater_idx.nextVal, '강변', 2, 3, '광진구');
+	insert into theater values(seq_theater_idx.nextVal, '건대입구', 2, 4, '광진구');
+	
+	select * from theater
+	
+	---------------------------------------------movie--------
+	
+	insert into movie values(seq_movie_idx.nextVal, '브로커', '2022-06-08', 80);
+	insert into movie values(seq_movie_idx.nextVal, '범죄도시2', '2022-05-18', 70);
+	insert into movie values(seq_movie_idx.nextVal, '마녀2', '2022-06-15', 60);
+
+	select * from movie
+
+
+
+
+
+
+
+*/
--- movieDB.sql
+++ movieDB.sql
@@ -1,6 +1,6 @@
 /*
-	-----------------------------------------------------------user--------
- 	create user
+	-------------------------------------------------------user_info--------
+ 	create table user_info
  	(
  		u_id 		varchar2(100) 	not null,	-- 아이디*
  		u_pwd 	varchar2(100)	not null,	-- 패스워드
@@ -10,12 +10,12 @@
  		u_email 	varchar2(100)				-- 이메일
  	)
  
- 	alter table user
+ 	alter table user_info
  		add constraints pk_user_u_id primary key(u_id)
  		
- 		
+ 	select * from user
  	--------------------------------------------------------ticketing--------
- 	create ticketing_info
+ 	create table ticketing_info
  	(
  		u_id 		varchar2(100) 	not null,	-- 아이디*
  		m_idx 	int 				not null,	-- 영화
@@ -26,21 +26,23 @@
  		ti_money int 				not null	-- 금액
  	)
  	
- 	alter table user
- 		add constraints fk_ticketing_info_u_id foreign key(u_id) references user(u_id)	
+ 	alter table ticketing_info
+ 		add constraints fk_ticketing_info_u_id foreign key(u_id) references user_info(u_id)	
  		
  	alter table ticketing_info
  		add constraints pk_ticketing_info_u_id primary key(u_id)
  		
- 	alter table user
+ 	alter table ticketing_info
  		add constraints fk_ticketing_info_m_idx foreign key(m_idx) references movie(m_idx)	
  		
- 	alter table user
+ 	alter table ticketing_info
  		add constraints fk_ticketing_info_th_idx foreign key(th_idx) references theater(th_idx)		
  		
  		
  	-------------------------------------------------------theater-------
- 	create theater
+ 	create sequence seq_theater_idx
+ 	
+ 	create table theater
  	(
  		th_idx 		int 				not null,	-- 번호*
  		th_name 		varchar2(100) 	not null,	-- 이름
@@ -52,12 +54,14 @@
  	alter table theater
  		add constraints pk_theater_th_idx primary key(th_idx)
  		
- 	alter table user
+ 	alter table theater
  		add constraints fk_theater_m_idx foreign key(m_idx) references movie(m_idx)	
  		
  		
  	-------------------------------------------------------movie--------
- 	create movie
+ 	create sequence seq_movie_idx
+ 	
+ 	create table movie
  	(
  		m_idx 		int 				not null,	-- 번호*
  		m_name 		varchar2(100) 	not null,	-- 이름
