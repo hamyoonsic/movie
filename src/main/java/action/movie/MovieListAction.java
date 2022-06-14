e5562957c60c4c4bde14d80caa08fa27841bc6e0
ict04-12 <ict04-12@DESKTOP-N1NQKIG>
2022-06-14 09:42:00 +0900

    movielist.do

--- src/main/java/action/movie/MovieListAction.java
+++ src/main/java/action/movie/MovieListAction.java
@@ -1,15 +1,12 @@
-package action.movie;
-
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.json.simple.JSONObject;
 
 import dao.MovieDao;
 import vo.MovieVo;
@@ -17,7 +14,6 @@ import vo.MovieVo;
 /**
  * Servlet implementation class MovieListAction
  */
-//메인페이지
 @WebServlet("/movie_list.do")
 public class MovieListAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -29,20 +25,18 @@ public class MovieListAction extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
+		
 		List<MovieVo> list	= MovieDao.getInstance().selectList();
 		
-		JSONObject json	=	new JSONObject();
+		request.setAttribute("list", list);
 		
-		json.put("m_list", list);
-		
-		String json_str	=	json.toJSONString();
-		
-		response.setContentType("text/json; charset=utf-8;");
-		response.getWriter().print(json_str);
-		
-		
+		//forward
+		String forward_page = "ticket_content.jsp";
+		RequestDispatcher disp = request.getRequestDispatcher(forward_page);
+		disp.forward(request, response);
 
 	}
 
 }
 
+	
